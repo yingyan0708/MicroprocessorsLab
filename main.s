0f774ba7ef738f3f2b884422f5db79b97951b537
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-11-01 12:02:10 +0000

    led

--- main.s
+++ main.s
@@ -1,41 +1,28 @@
 	#include <xc.inc>
 	
-psect	code, abs
-main:
-	org 0x0
-	goto	setup
-	
-	org 0x100		    ; Main code starts here at address 0x100
+	psect	code, abs
 
-	; ******* Programme FLASH read Setup Code ****  
-setup:	
-	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
+main:
+	org	0x0
 	goto	start
-	; ******* My data and where to put it in RAM *
-myTable:
-	db	'T','h','i','s',' ','i','s',' ','j','u','s','t'
-	db	' ','s','o','m','e',' ','d','a','t','a'
-	myArray EQU 0x400	; Address in RAM for data
-	counter EQU 0x10	; Address of counter variable
-	align	2		; ensure alignment of subsequent instructions 
-	; ******* Main programme *********************
-start:	
-	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A	; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A	; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A	; load low byte to TBLPTRL
-	movlw	22		; 22 bytes to read
-	movwf 	counter, A	; our counter register
+
+	org	0x100		    ; Main code starts here at address 0x100
+start:
+	movlw 	0x0
+	movwf	TRISC, A	    ; Port C all outputs
+	bra 	test
 loop:
-        tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-	decfsz	counter, A	; count down to zero
-	bra	loop		; keep going until finished
+	movff 	0x06, PORTC
+	incf 	0x06, W, A
+	call	delay
 	
-	goto	0
+test:
+	movwf	0x06, A	    ; Test for end of loop condition
+	movlw 	0x63
+	cpfsgt 	0x06, A
+	bra 	loop	    ; Not yet finished goto start of loop again
+	goto 	0x0		    ; Re-run program from start
+delay:
+	decfsz	0x63, A	    ; Decrement
 
 	end	main
