8c59bf2df8a59770d691146e81d10d1d3d1fc632
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-11-05 10:49:16 +0000

    led display

--- main.s
+++ main.s
@@ -7,22 +7,36 @@ main:
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-	call	delay
-	
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop	    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-delay:
-	decfsz	0x63, A	    ; Decrement
+table:	
+	db	0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07	;Table loading with 8 bytes
+	counter	EQU 0x10 ; Address of counter variable
+	num	EQU 8 ;counter
+	align	2 ; ensure alignment of subsequent instructions
 
-	end	main
+start:	
+	clrf	TRISC, A ;clears PORTC
+	movlw	low highword(table) ; address of data in PM
+	movwf	TBLPTRU, A ; load upper bits to TBLPTRU
+	movlw	high(table) ; address of data in PM
+	movwf	TBLPTRH, A ; load high byte to TBLPTRH
+	movlw	low(table) ; address of data in PM
+	movwf	TBLPTRL, A ; load low byte to TBLPTRL
+	movlw	num ; 8 bytes to read
+	movwf	counter, A ; our counter register
+	
+loop:
+	tblrd*+ ; move one byte from PM to TABLAT, 					
+		; increment TBLPRT
+	movff 	TABLAT, PORTC ; move read data from TABLAT to
+			    ; PORTC, increment PORTC	
+	movlw	0x0f ;all bits in
+	movwf	0x20, A ;stores value in address 0x20
+	call	delay
+	decfsz	counter, A ; count down to zero
+	bra	loop ; keep going until finished
+	goto	start
+	
+delay:
+	decfsz	0x20,A    ; Decrement until zero
+	bra	delay
+	return
