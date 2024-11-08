2e7d2db9e2acb2281c72e060250aebe76bce0ad4
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-11-08 12:02:50 +0000

    SPI code

--- main.s
+++ main.s
@@ -4,7 +4,7 @@
 
 main:
 	org	0x0
-	goto	start
+	goto	setup
 
 	org	0x100		    ; Main code starts here at address 0x100
 table:	
@@ -13,9 +13,10 @@ table:
 	num	EQU 8 ;counter
 	align	2 ; ensure alignment of subsequent instructions
 
+setup:
+	call	SPI_MasterInit ; call master_init function
+	
 start:	
-	clrf	TRISD, A ;clears PORTD
-	clrf	TRISC, A ;clears PORTC
 	movlw	low highword(table) ; address of data in PM
 	movwf	TBLPTRU, A ; load upper bits to TBLPTRU
 	movlw	high(table) ; address of data in PM
@@ -26,18 +27,42 @@ start:
 	movwf	counter, A ; our counter register
 
 loop:
-	tblrd*+ ; move one byte from PM to TABLAT, 					
-		; increment TBLPRT
-	movff 	TABLAT, PORTD ; move read data from TABLAT to
-			    ; PORTD, increment PORTD	
-	movlw	0x0f ;all bits in
-	movwf	0x20, A ;stores value in address 0x20
-	call	delay
-	decfsz	counter, A ; count down to zero
+	call	SPI_MasterTransmit ; call SPI_MasterTransmit function
+	decfsz	counter, F, A ; count down to zero
 	bra	loop ; keep going until finished
 	goto	start
 	
+SPI_MasterTransmit:
+	tblrd*+ ; move one byte from PM to TABLAT, 					
+		; increment TBLPRT
+	movf 	TABLAT, W, A ; move read data from TABLAT to W register
+	movwf 	SSP2BUF, A 	; write data to output buffer
+	call	Wait_Transmit ; call wait_transmit function
+	movlw	0x0f                                                                                   ;load delay value
+	movwf	0x20, A ;stores value in address 0x20
+	call	delay ;call delay
+	call	delay
+	return
+	
+
+SPI_MasterInit:	; Set Clock edge to negative	
+	bcf	CKE2	; CKE bit in SSP2STAT
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw 	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+	movwf 	SSP2CON1, A	; SDO2 output; SCK2 output
+	setf	TRISD, A
+	bcf	TRISD, PORTD_SDO2_POSN, A	; SDO2 output	
+	bcf	TRISD, PORTD_SCK2_POSN, A	; SCK2 output	
+	return 
+
+
+Wait_Transmit:	; Wait for transmission to complete	
+	btfss 	PIR2, 5, A		; check interrupt flag to see if data has been sent                                                                                                                                                                                                                                    
+	bra 	Wait_Transmit
+	bcf 	PIR2, 5, A		; clear interrupt flag	
+	return 
+	
 delay:
-	decfsz	0x20,A    ; Decrement until zero
+	decfsz	0x20, F, A    ; Decrement until zero
 	bra	delay
 	return
