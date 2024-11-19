9e627dc23175babbcb0e5bd7792e55c41689c02f
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-11-19 12:19:43 +0000

    Write keypad input to LCD

--- KeyPad.s
+++ KeyPad.s
@@ -26,45 +26,138 @@ KeyPad_Read:
     movf    PORTE, W, A
     addwf   ROW, F, A
     movff   ROW, PORTH
-    return
+    bra	    check_1
+    ;return
     
 check_1:
     movlw   01110111B ;1
     cpfseq  ROW, A
     bra	    check_2
     retlw   '1'
-check_2:
-;    01111011B ;2
-;    01111101B ;3
-;    01111110B ;F
-;    10110111B ;4
-;    10111011B ;5
-;    10111101B ;6
-;    10111110B ;E
-;    11010111B ;7
-;    11011011B ;8
-;    11011101B ;9
-;    11011110B ;D
-;    11100111B ;A
-;    11101011B ;0
-;    11101101B ;B
-;    11101110B ;C
-;    11111111B ;null
-
-
- 
-    
-	
-    bsf	    SPEN	; enable
-    bcf	    SYNC	; synchronous
-    bcf	    BRGH	; slow speed
-    bsf	    TXEN	; enable transmit
-    bcf	    BRG16	; 8-bit generator only
-    movlw   103		; gives 9600 Baud rate (actually 9615)
-    movwf   SPBRG1, A	; set baud rate
-    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin
-					; must set TRISC6 to 1
     return
+    
+check_2:
+    movlw   10110111B ;2
+    cpfseq  ROW, A
+    bra	    check_3
+    retlw   '2'
+    return
+    
+check_3:
+    movlw   11010111B ;3
+    cpfseq  ROW, A
+    bra	    check_4
+    retlw   '3'
+    return
+    
+check_4:
+    movlw   01111011B ;4
+    cpfseq  ROW, A
+    bra	    check_5
+    retlw   '4'
+    return
+    
+check_5:
+    movlw   10111011B ;5
+    cpfseq  ROW, A
+    bra	    check_6
+    retlw   '5'
+    return
+    
+check_6:
+    movlw   11011011B ;6
+    cpfseq  ROW, A
+    bra	    check_7
+    retlw   '6'
+    return
+    
+check_7:
+    movlw   01111101B ;7
+    cpfseq  ROW, A
+    bra	    check_8
+    retlw   '7'
+    return
+    
+check_8:
+    movlw   10111101B ;8
+    cpfseq  ROW, A
+    bra	    check_9
+    retlw   '8'
+    return
+
+check_9:
+    movlw   11011101B ;9
+    cpfseq  ROW, A
+    bra	    check_0
+    retlw   '9'
+    return
+    
+check_0:
+    movlw   10111110B ;0
+    cpfseq  ROW, A
+    bra	    check_A
+    retlw   '0'
+    return
+    
+check_A:
+    movlw   01111110B ;A
+    cpfseq  ROW, A
+    bra	    check_B
+    retlw   'A'
+    return
+    
+check_B:
+    movlw   11011110B ;B
+    cpfseq  ROW, A
+    bra	    check_C
+    retlw   'B'
+    return
+    
+check_C:
+    movlw   11101110B ;C
+    cpfseq  ROW, A
+    bra	    check_D
+    retlw   'C'
+    return
+    
+check_D:
+    movlw   11101101B ;D
+    cpfseq  ROW, A
+    bra	    check_E
+    retlw   'D'
+    return
+    
+check_E:
+    movlw   11101011B ;E
+    cpfseq  ROW, A
+    bra	    check_F
+    retlw   'E'
+    return
+    
+check_F:
+    movlw   11100111B ;F
+    cpfseq  ROW, A
+    bra	    check_null
+    retlw   'F'
+    return
+
+check_null:
+    movlw   11111111B ;null
+    cpfseq  ROW, A  
+    goto    KeyPad_Read
+    retlw   'N'
+    return
+
+
+    ;bsf	    SPEN	; enable
+    ;bcf	    SYNC	; synchronous
+    ;bcf	    BRGH	; slow speed
+    ;bsf	    TXEN	; enable transmit
+    ;cf	    BRG16	; 8-bit generator only
+    ;movlw   103		; gives 9600 Baud rate (actually 9615)
+    ;movwf   SPBRG1, A	; set baud rate
+    ;bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin
+					; must set TRISC6 to 1
 
 KeyPad_delay:	    ; Message stored at FSR2, length stored in W
     movlw   50
--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_clear, second_line
+extrn	LCD_Setup, LCD_Write_Message, LCD_clear, second_line, LCD_Send_Byte_D
 extrn	KeyPad_Setup, KeyPad_Read
 	
 psect	udata_acs   ; reserve data space in access ram
@@ -10,6 +10,7 @@ delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
+myKeyPad:   ds 1
 
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
@@ -18,6 +19,7 @@ myTable:
 					; message, plus carriage return
 	myTable_l   EQU	13	; length of data
 	button	    EQU 0
+	myKeyPad_1  EQU 1
 	align	2
     
 psect	code, abs	
@@ -26,45 +28,56 @@ rst: 	org 0x0
 
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	
-	clrf	PORTD			;clear port D
+	clrf	PORTD, A			;clear port D
 	movlw	0x01			; all bits in set RD0 as input
 	movwf 	TRISD, A		; Port D Direction Register
-
+	
 	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
 	call	UART_Setup	; setup UART
 	call	KeyPad_Setup
-	call	LCD_Setup	; setup UART
+	call	LCD_Setup	; setup LCD
 	goto	start
 	
 	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
+start: 	
+	;lfsr	0, myKeyPad	; Load FSR0 with address in RAM	
+	;movlw	low highword(myTable)	; address of data in PM
+	;movwf	TBLPTRU, A		; load upper bits to TBLPTRU
+	;movlw	high(myTable)	; address of data in PM
+	;movwf	TBLPTRH, A		; load high byte to TBLPTRH
+	;movlw	low(myTable)	; address of data in PM
+	;movwf	TBLPTRL, A		; load low byte to TBLPTRL
+	;movlw	myTable_l	; bytes to read
+	;movwf 	counter, A		; our counter register
 	
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
+;loop: 	;tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	;movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	;decfsz	counter, A		; count down to zero
+	;bra	loop		; keep going until finished
+	
+	;movlw	myTable_l	; output message to UART
+	;lfsr	2, myArray
+	
+	;lfsr	2, myKeyPad
+	call	KeyPad_Read
+	call	LCD_Send_Byte_D
+	;movwf	INDF2
+	goto	start
+	;movlw	myKeyPad_1
+	;call	UART_Transmit_Message
 
-	movlw	myTable_l	; output message to LCD
-	addlw	0xff		; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
+	;movlw	myTable_l	; output message to LCD
+	;addlw	0xff		; don't send the final carriage return to LCD
+	;lfsr	2, myKeyPad
+	;call	LCD_Write_Message
+	nop
+	goto	start
 
 reset_button:
-	call	KeyPad_Read
-	btfss	PORTD, button
-	goto	reset_button
+	;call	KeyPad_Read
+	;btfss	PORTD, button
+	;goto	reset_button
 	call	LCD_clear
 	call	second_line
 	goto	start
