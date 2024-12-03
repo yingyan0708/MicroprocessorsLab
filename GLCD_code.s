ab33955899a02417988c2a875e40db68337b8f53
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-12-03 10:39:26 +0000

    display

--- GLCD_code.s
+++ GLCD_code.s
@@ -1,145 +1,73 @@
 #include <xc.inc>
 
-global  LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_D
+global init_LCD, send_data, send_command,CS1,CS2
 
-psect	udata_acs   ; named variables in access ram
-LCD_cnt_l:	ds 1	; reserve 1 byte for variable LCD_cnt_l
-LCD_cnt_h:	ds 1	; reserve 1 byte for variable LCD_cnt_h
-LCD_cnt_ms:	ds 1	; reserve 1 byte for ms counter
-LCD_tmp:	ds 1	; reserve 1 byte for temporary use
-LCD_counter:	ds 1	; reserve 1 byte for counting through nessage
+psect	udata_acs
+RS  EQU	2 ;port B is control line
+R   EQU 3 ;RW
+EN  EQU	4
+CS1 EQU	0
+CS2 EQU	1
+RST EQU	5
 
-PSECT	udata_acs_ovr,space=1,ovrld,class=COMRAM
-LCD_hex_tmp:	ds 1    ; reserve 1 byte for variable LCD_hex_tmp
-
-	LCD_E	EQU	4	; LCD enable bit
-    	LCD_RS	EQU	2	; LCD register select bit
-	R	EQU	3 ;RW
-	CS1	EQU	0
-	CS2	EQU	1
-	RST	EQU	5
-
-psect	lcd_code,class=CODE
-    
+psect	glcd,class=CODE
+   
 init_LCD:
-	clrf	TRISD
-	clrf	LATD
-	clrf	LATB
-	bsf	PORTB, RST ;reset
-	call	delay
-	bsf	PORTB, CS1 ;select left half
-	bsf	PORTB, CS2 ;select right half
-	call	delay
+	clrf	PORTB, A
+	clrf	PORTD, A
+	clrf	LATD, A ;clear PORTD output latch (data lines)
+	clrf	LATB, A ;clear PORTB output latch (control lines)
+	clrf	TRISD, A ;Set all PORTD pins as outputs (data lines)
+	clrf	TRISB, A
+
+	bcf	PORTB, RST
+	nop ;delay
+	bsf	PORTB, RST
+	nop ;delay
+
+	movlw	0x3F ; Function Set: 8-bit, 128x64 resolution, normal display
+	call	send_command
+
 	movlw	0xB8 ;set to page 0 (x-address)
 	call	send_command
+
 	movlw	0x40 ;set to strip 0 in page (y-address)
 	call	send_command
+
 	movlw	0xC0 ;start line, start from row 0 (z-address)
 	call	send_command
-	movlw	0x3F ;display on
-	call	send_command
+
+	bcf	PORTB, CS2 
+	nop
+	bsf	PORTB, CS1;ft half active
+	nop
 	return
 
-	
 send_command: ;RS and R/W are both 0 when sending command
-	bcf	LATB, RS, A ;clear
-	bcf	LATB, R, A ; clear RW
+	bcf	PORTB, RS, A ;clear RS
+	bcf	PORTB, R, A ;clear RW
 	movwf	PORTD ;store command in w, move command to port D where port D is data line
-	bsf	LATB, EN; set Enable pin to 1
-	call	delay
+	bsf	PORTB, EN ;enable pin to 1
+	nop
+	nop
 	bcf	PORTB, EN
-	call	delay
-	return
-	
-LCD_Write_Message:	    ; Message stored at FSR2, length stored in W
-	movwf   LCD_counter, A
-LCD_Loop_message:
-	movf    POSTINC2, W, A
-	call    LCD_Send_Byte_D
-	decfsz  LCD_counter, A
-	bra	LCD_Loop_message
+	nop
+	nop
 	return
 
-LCD_Send_Byte_I:	    ; Transmits byte stored in W to instruction reg
-	movwf   LCD_tmp, A
-	swapf   LCD_tmp, W, A   ; swap nibbles, high nibble goes first
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bcf	LATB, LCD_RS, A	; Instruction write clear RS bit
-	bcf	LATB, R, A
-	call    LCD_Enable  ; Pulse enable Bit 
-	movf	LCD_tmp, W, A   ; swap nibbles, now do low nibble
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bcf	LATB, LCD_RS, A	; Instruction write clear RS bit
-        call    LCD_Enable  ; Pulse enable Bit 
+send_data: ;when writing/sending data, RS pin is set to 1
+	bsf	PORTB, RS
+	bcf	PORTB, R
+	movwf	PORTD ;place data on port D
+	bsf	PORTB, EN
+	nop
+	nop
+	bcf	PORTB, EN
+	nop
+	nop
 	return
-
-LCD_Send_Byte_D:	    ; Transmits byte stored in W to data reg
-	movwf   LCD_tmp, A
-	swapf   LCD_tmp, W, A	; swap nibbles, high nibble goes first
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bsf	LATB, LCD_RS, A	; Data write set RS bit
-	bcf	LATB, R
-	call    LCD_Enable  ; Pulse enable Bit 
-	movf	LCD_tmp, W, A	; swap nibbles, now do low nibble
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bsf	LATB, LCD_RS, A	; Data write set RS bit	    
-        call    LCD_Enable  ; Pulse enable Bit 
-	movlw	10	    ; delay 40us
-	call	LCD_delay_x4us
-	return
-
-LCD_Enable:	    ; pulse enable bit LCD_E for 500ns
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	bsf	LATB, LCD_E, A	    ; Take enable high
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	bcf	LATB, LCD_E, A	    ; Writes data to LCD
-	return
-    
-; ** a few delay routines below here as LCD timing can be quite critical ****
-LCD_delay_ms:		    ; delay given in ms in W
-	movwf	LCD_cnt_ms, A
-lcdlp2:	movlw	250	    ; 1 ms delay
-	call	LCD_delay_x4us	
-	decfsz	LCD_cnt_ms, A
-	bra	lcdlp2
-	return
-    
-LCD_delay_x4us:		    ; delay given in chunks of 4 microsecond in W
-	movwf	LCD_cnt_l, A	; now need to multiply by 16
-	swapf   LCD_cnt_l, F, A	; swap nibbles
-	movlw	0x0f	    
-	andwf	LCD_cnt_l, W, A ; move low nibble to W
-	movwf	LCD_cnt_h, A	; then to LCD_cnt_h
-	movlw	0xf0	    
-	andwf	LCD_cnt_l, F, A ; keep high nibble in LCD_cnt_l
-	call	LCD_delay
-	return
-
-LCD_delay:			; delay routine	4 instruction loop == 250ns	    
-	movlw 	0x00		; W=0
-lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
-	subwfb 	LCD_cnt_h, F, A	; no carry when 0x00 -> 0xff
-	bc 	lcdlp1		; carry, then loop again
-	return			; carry reset so return
-
 
 end
 
 
+	
--- main.s
+++ main.s
@@ -1,54 +1,41 @@
 #include <xc.inc>
 
-extrn	init_LCD, send_data
-    
+extrn	init_LCD, send_data, send_command
+   
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
+count:	    ds 1
     
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
-    
-psect	code, abs	
-rst: 	org 0x0
- 	goto	setup
-	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
+psect code, abs
+rst: 
+	org	0x0
+	goto	setup
+; ******* Programme FLASH read Setup Code ***********************
+setup:
+	bcf	CFGS; point to Flash program memory  
+	bsf	EEPGD ; access Flash program memory
 	call	init_LCD
 	goto	start
+; ******* Main programme ****************************************
+start: ;display character A
+	movlw	0xB8 ;set to page 0 (x-address)
+	call	send_command
+
+	movlw	0x40 ;set to strip 0 in page (y-address)
+	call	send_command
 	
-	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	;movlw	myTable_l	; output message to UART
-	;lfsr	2, myArray
-	;call	UART_Transmit_Message
-	movlw	myTable_l	; output message to LCD
-	addlw	0xff		; don't send the final carriage return to LCD
-	lfsr	2, myArray
+	movlw	0x40
+	movwf	count, A
+loop:	
+	decfsz	count
+	call	clear
+	bra	loop
+	
+clear:
+	movlw	0xFF
 	call	send_data
-	goto	$		; goto current line in code
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
 	return
+	
+
+end	rst
