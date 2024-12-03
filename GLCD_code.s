cb60b4c99899d56c0c1085a8ea936fa34adf4ae0
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-12-03 15:37:24 +0000

    successful try

--- GLCD_code.s
+++ GLCD_code.s
@@ -3,6 +3,9 @@
 global init_LCD, send_data, send_command,CS1,CS2
 
 psect	udata_acs
+LCD_cnt_l:	ds 1   ; reserve 1 byte for variable LCD_cnt_l
+LCD_cnt_h:	ds 1   ; reserve 1 byte for variable LCD_cnt_h
+
 RS  EQU	2 ;port B is control line
 R   EQU 3 ;RW
 EN  EQU	4
@@ -53,24 +56,6 @@ send_command: ;RS and R/W are both 0 when sending command
 	nop
 	movwf	PORTD ;store command in w, move command to port D where port D is data line
 	bsf	PORTB, EN ;enable pin to 1
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
 	nop
 	nop
 	nop
@@ -166,9 +151,26 @@ send_data: ;when writing/sending data, RS pin is set to 1
 	nop
 	nop
 	nop
+	movlw	10
+	call	LCD_delay_x4us
 	return
 
+LCD_delay_x4us:		    ; delay given in chunks of 4 microsecond in W
+	movwf	LCD_cnt_l, A	; now need to multiply by 16
+	swapf   LCD_cnt_l, F, A	; swap nibbles
+	movlw	0x0f	    
+	andwf	LCD_cnt_l, W, A ; move low nibble to W
+	movwf	LCD_cnt_h, A	; then to LCD_cnt_h
+	movlw	0xf0	    
+	andwf	LCD_cnt_l, F, A ; keep high nibble in LCD_cnt_l
+	call	LCD_delay
+	return
+
+LCD_delay:			; delay routine	4 instruction loop == 250ns	    
+	movlw 	0x00		; W=0
+lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
+	subwfb 	LCD_cnt_h, F, A	; no carry when 0x00 -> 0xff
+	bc 	lcdlp1		; carry, then loop again
+	return			; carry reset so return
+
 end
-
-
-	
--- main.s
+++ main.s
@@ -1,30 +1,31 @@
 #include <xc.inc>
 
 extrn	init_LCD, send_data, send_command
-   
+
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
 count:	    ds 1
-    
+
 psect code, abs
-rst: 
+rst:
 	org	0x0
 	goto	setup
-; ******* Programme FLASH read Setup Code ***********************
+
+; ******* Program Setup Code ***********************
 setup:
-	bcf	CFGS; point to Flash program memory  
+	bcf	CFGS ; point to Flash program memory
 	bsf	EEPGD ; access Flash program memory
 	call	init_LCD
 	call	clear_display
 	goto	start
-; ******* Main programme ****************************************
-	
+
+; ******* Main program ****************************************
 start:
-	movlw	0xB8 ;set to page 0 (x-address)
+	movlw	0xB8 ; set to page 0 (x-address)
 	call	send_command
 
-	movlw	0x40 ;set to strip 0 in page (y-address)
+	movlw	0x40 ; set to strip 0 in page (y-address)
 	call	send_command
 	
 	movlw	11111000B
@@ -54,44 +55,77 @@ start:
 	goto	halt_program
 
 halt_program:
-	; Add an infinite loop to stop the program from running
+	; Infinite loop to halt program execution
+
 	goto	halt_program
-	
-    
-clear_display: ;display character A
-	movlw	0xB8 ;set to page 0 (x-address)
-	call	send_command
 
-	movlw	0x40 ;set to strip 0 in page (y-address)
+; ******* Clear Display Routine ***********************
+clear_display:
+	; Clear pages and reset cursor for each page (0xB8 to 0xBF)
+	movlw	0xB8 ; Set to page 0 (x-address)
 	call	send_command
-	
-	movlw	0x40
-	movwf	count, A
-loop1:	
-	call	clear
-	decfsz	count
-	bra	loop1
-	goto	page2
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
 
-page2:
-	movlw	0xB9 ;set to page 0 (x-address)
+	movlw	0xB9 ; Set to page 1 (x-address)
 	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
 
-	movlw	0x40 ;set to strip 0 in page (y-address)
+	movlw	0xBA ; Set to page 2 (x-address)
 	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
+
+	movlw	0xBB ; Set to page 3 (x-address)
+	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
+
+	movlw	0xBC ; Set to page 4 (x-address)
+	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
+
+	movlw	0xBD ; Set to page 5 (x-address)
+	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
+
+	movlw	0xBE ; Set to page 6 (x-address)
+	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
+
+	movlw	0xBF ; Set to page 7 (x-address)
+	call	send_command
+	movlw	0x40 ; Set to strip 0 in page (y-address)
+	call	send_command
+	call	clear_page
 	
-	movlw	0x40
-	movwf	count, A
-loop2:	
-	call	clear
-	decfsz	count
-	bra	loop2
 	return
-	
+
+; Routine to clear a page (sets column address to 0x40 and sends 0x00 to clear)
+clear_page:
+	movlw	0x40 ; Set column address to the beginning of the page
+	movwf	count
+clear_loop:
+	call	clear
+	decfsz	count
+	bra	clear_loop
+	return
+
+; Routine to send a clear command (send 0x00 data to clear the display)
 clear:
 	movlw	0x00
 	call	send_data
 	return
-	
 
-end	rst
+end
