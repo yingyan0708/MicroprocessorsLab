e985b3d6b8e757f980d924b5c4473394bff68926
yyc22 <yyc22@sklb-l1wolf-060.lib.ic.ac.uk>
2024-12-09 18:38:30 +0000

    some change on buzzer configuration

--- Buzzer.s
+++ Buzzer.s
@@ -5,15 +5,30 @@ psect	udata_acs
 psect	buzzer_code, class=CODE
 
 PWM_Setup:
+    ;set up PR2 value 
+    banksel PR2
     movlw   0xFF
-    movwf   PR2
-
-    bsf	    CCP4CON, 4
-    bsf	    CCP4CON, 5
-    CCPR4L
+    movwf   PR2, B
+    
+    ;set up PWM duty cycle
+    banksel CCP4CON
+    banksel CCPR4L
+    bcf	    CCP4CON, 4
+    bcf	    CCP4CON, 5
+    movlw   10000000B
+    movwf   CCPR4L, B
     
     ;Prescale value 16
-    movlw   00010000B ;prescale value
-    movwf   TMR2
+    banksel TMR2
+    movlw   00010000B ;prescale value = 16
+    movwf   TMR2, B
     
+    banksel T2CON
     bsf	    T2CON, 2
+    
+    banksel CCP4CON
+    movlw   0x0C ;CCP4M = 1100
+    movwf   CCP4CON, B
+    
+    bcf	    TRSIB, 6 ;set RB6 as output for piezo buzzer
+    
