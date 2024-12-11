435a78498d343910f52de88d83dd78c387c18f2b
cid02236328 <148618488+cid02236328@users.noreply.github.com>
2024-12-11 11:35:01 +0000

    Create bitmap.s

---
+++ bitmap.s
@@ -0,0 +1,164 @@
+num0:
+    movlw   00000000B
+    call    GLCD_SendCommand
+    movlw   00111100B
+    call    GLCD_SendCommand
+    movlw   01000010B
+    call    GLCD_SendCommand
+    movlw   10000001B
+    call    GLCD_SendCommand
+    movlw   10000001B
+    call    GLCD_SendCommand
+    movlw   01000010B
+    call    GLCD_SendCommand
+    movlw   00111100B
+    call    GLCD_SendCommand
+    movlw   00000000B
+    call    GLCD_SendCommand
+
+num1:
+    movlw   00000000B
+    call    GLCD_SendCommand
+    movlw   00000000B
+    call    GLCD_SendCommand
+    movlw   00000001B
+    call    GLCD_SendCommand
+    movlw   00000001B
+    call    GLCD_SendCommand
+    movlw   11111111B
+    call    GLCD_SendCommand
+    movlw   10000001B
+    call    GLCD_SendCommand
+    movlw   01000001B
+    call    GLCD_SendCommand
+    movlw   00000000B
+    call    GLCD_SendCommand
+
+num2:
+    movlw   11110001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10011111B
+    call    GLCD_SendCommand
+
+num3:
+    movlw   11111111B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+
+num4:
+    movlw   11111111B
+    call    GLCD_SendCommand
+    movlw   00010000B
+    call    GLCD_SendCommand
+    movlw   00010000B
+    call    GLCD_SendCommand
+    movlw   00010000B
+    call    GLCD_SendCommand
+    movlw   00010000B
+    call    GLCD_SendCommand
+    movlw   00010000B
+    call    GLCD_SendCommand
+    movlw   00010000B
+    call    GLCD_SendCommand
+    movlw   11110000B
+    call    GLCD_SendCommand
+    
+num5:
+    movlw   10011111B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10010001B
+    call    GLCD_SendCommand
+    movlw   10011111B
+    call    GLCD_SendCommand
+
+num6:
+11111110
+10000000
+10000000
+11111100
+10000010
+10000010
+10000010
+11111100
+
+num7:
+11111111
+10000010
+10000010
+10000010
+10000010
+10000000
+10000000
+10000000
+
+num8:
+11111100
+10000010
+10000010
+11111100
+10000010
+10000010
+10000010
+11111100
+
+num9:
+11111100
+10000010
+10000010
+11111110
+10000010
+10000010
+10000010
+11111100
+
+degrees:
+
+letterC:
+
+letterT:
+
+letterE:
+
+letterM:
+
+letterP:
+
+letterR:
+
+letterU:
+
