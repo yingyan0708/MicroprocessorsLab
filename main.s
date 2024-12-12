6da57ed886013c8274108466d52019bb3c09f541
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-12-12 15:30:35 +0000

    UART change to next line

--- main.s
+++ main.s
@@ -5,7 +5,11 @@ extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
-    
+first_digit:ds 1
+second_digit: ds 1
+third_digit: ds 1
+forth_digit: ds 1
+
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
 
@@ -14,7 +18,7 @@ psect	data
 myTable:
 	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
 					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
+	myTable_l   EQU	5	; length of data
 	align	2
     
 psect	code, abs	
@@ -29,19 +33,19 @@ setup:	bcf	CFGS	; point to Flash program memory
 	
 	; ******* Main programme ****************************************
 start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
+	movlw	0x30
+	movwf	first_digit, A
+	movff   first_digit, POSTINC0
+	movlw	0x31
+	movwf	second_digit, A
+	movff	second_digit, POSTINC0
+	movlw   0x0D              ; ASCII for Carriage Return (CR)
+	movwf   POSTINC0          ; Add CR to myArray
+	movlw   0x0A              ; ASCII for Line Feed (LF)
+	movwf   POSTINC0          ; Add LF to myArray
+	movlw	0x32
+	movwf	third_digit, A
+	movff	third_digit, POSTINC0	
 	movlw	myTable_l	; output message to UART
 	lfsr	2, myArray
 	call	UART_Transmit_Message
